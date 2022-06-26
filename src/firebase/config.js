9243e5e930ab7c93df09183be49552b6c38a2c7e
KMinh <daukhacminh006@gmail.com>
2022-06-26 10:41:42 +0700

    Commit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAVY5nrYgO1QgMImuO59UroX034T3kAhvo",
-  authDomain: "todo-list-bcf38.firebaseapp.com",
-  projectId: "todo-list-bcf38",
-  storageBucket: "todo-list-bcf38.appspot.com",
-  messagingSenderId: "892246226411",
-  appId: "1:892246226411:web:1ea982007fd182880995ae"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
